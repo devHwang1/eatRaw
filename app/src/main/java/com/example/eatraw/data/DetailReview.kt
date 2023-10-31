8c0dd019332d05f141116e40baddb81411fd320e
zinc1004 <roes2003@naver.com>
2023-10-31 16:51:49 +0900

    10.31 / 리뷰리스트 및 쓰기페이지 디자인 완료 (별부분은 오류가 나서 잠시보류)

---
+++ app/src/main/java/com/example/eatraw/data/DetailReview.kt
@@ -0,0 +1,10 @@
+package com.example.eatraw.data
+
+class DetailReview (
+    val MenuResource: Int,
+    val fishiName: String,
+    val fishPrice: Int,
+    val memberName: String,
+    val score: Int,
+    val textcontent: String
+)
