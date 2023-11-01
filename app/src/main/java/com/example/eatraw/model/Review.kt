70450e7f6b44eb4bb556f951e52100e42b44be95
parkys95 <pys0938@naver.com>
2023-11-01 09:20:00 +0900

    11 01 다합침

--- app/src/main/java/com/example/eatraw/model/Review.kt
+++
@@ -1,9 +0,0 @@
-package com.example.eatraw.data
-
-data class Review(
-    val content: String,
-    val marketName: String,
-    val storeImg: String?,
-    val storeName: String,
-    val rating: Double?
-)
