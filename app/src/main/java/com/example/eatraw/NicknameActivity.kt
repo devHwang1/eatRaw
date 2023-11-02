dd3a169142f06fc5151707bd504c56b7d73cc745
parkys95 <pys0938@naver.com>
2023-11-02 12:06:05 +0900

    가입 페이지 원복

--- app/src/main/java/com/example/eatraw/NicknameActivity.kt
+++ app/src/main/java/com/example/eatraw/NicknameActivity.kt
@@ -1,11 +1,28 @@
 package com.example.eatraw
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.eatraw.databinding.ActivityNicknameBinding
 
 class NicknameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNicknameBinding
+    private lateinit var Nickname: String
+    private lateinit var thumNail: String
+    private lateinit var btn_register: Button
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityNicknameBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nickname)
+        setContentView(binding.root)
+
+        btn_register = findViewById(R.id.btn_register)
+
+        btn_register.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
