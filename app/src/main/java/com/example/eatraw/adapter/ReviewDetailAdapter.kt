048b6aa01a24a023a4aaf5ff9610fb2360dcafea
zinc1004 <roes2003@naver.com>
2023-11-03 09:40:27 +0900

    no message

--- app/src/main/java/com/example/eatraw/adapter/ReviewDetailAdapter.kt
+++ app/src/main/java/com/example/eatraw/adapter/ReviewDetailAdapter.kt
@@ -7,29 +7,38 @@ import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.eatraw.R
 import com.example.eatraw.data.DetailReview
+import com.example.eatraw.data.Review
+import com.example.eatraw.data.Users
 
-
-class ReviewDetailAdapter(private val detailReviews: List<DetailReview>) :
+//사용 데이터 : Review
+class ReviewDetailAdapter(private val review: List<Review>) :
     RecyclerView.Adapter<ReviewDetailAdapter.ReviewDetailAdapterViewHolder>() {
+
     class ReviewDetailAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView : ImageView = itemView.findViewById(R.id.Reviewimg)
         val menuFishName : TextView = itemView.findViewById(R.id.MenuFishName)
         val fishPrice : TextView = itemView.findViewById(R.id.StorePriceInt)
-        val memberName : TextView = itemView.findViewById(R.id.mName)
+        val likecounter : TextView = itemView.findViewById(R.id.likeInt)
         val starScore: TextView = itemView.findViewById(R.id.mStarsocore)
         val ratingBar : RatingBar = itemView.findViewById(R.id.DratingBar)
-        val contentView : TextView = itemView.findViewById(R.id.contentView)
-        val Textcomparison : TextView = itemView.findViewById(R.id.Textcomparison)
+        val reviewContent : TextView = itemView.findViewById(R.id.contentView)
+//        val Textcomparison : TextView = itemView.findViewById(R.id.Textcomparison)
 
-        //가격
-        val minPrice : TextView = itemView.findViewById(R.id.IntMin)
-        val maxPrice : TextView = itemView.findViewById(R.id.IntMax)
-        val avgPrice : TextView = itemView.findViewById(R.id.IntAvg)
+
+//        //user
+//        val memberName : TextView = itemView.findViewById(R.id.mName)
+//
+//        //가격
+//        val minPrice : TextView = itemView.findViewById(R.id.IntMin)
+//        val maxPrice : TextView = itemView.findViewById(R.id.IntMax)
+//        val avgPrice : TextView = itemView.findViewById(R.id.IntAvg)
 
 
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int ): ReviewDetailAdapterViewHolder {
         val view =
@@ -38,55 +47,69 @@ class ReviewDetailAdapter(private val detailReviews: List<DetailReview>) :
     }
 
     override fun onBindViewHolder(holder: ReviewDetailAdapterViewHolder, position: Int) {
-        val reviewDetail = detailReviews[position]
+        val review = review[position]
+//        val user = user[position]
+//        val reviewDetail = detailreview[position]
 
-        holder.imageView.setImageResource(reviewDetail.MenuResource)
-        holder.menuFishName.text = reviewDetail.fishiName
-        holder.fishPrice.text = reviewDetail.fishPrice.toString()
-        holder.memberName.text = reviewDetail.memberName
-        holder.starScore.text = reviewDetail.score.toString()
-        holder.contentView.text = reviewDetail.textcontent.toString()
+        // 리뷰 데이터를 뷰에 연결
+        // 이미지 URL을 Glide로 로드
+        review.storeImg?.let {
+            Glide.with(holder.itemView)
+                .load(it)
+                .into(holder.imageView)
+        }
 
-        holder.minPrice.text = reviewDetail.minPrice.toString()
-        holder.maxPrice.text = reviewDetail.maxPrice.toString()
-        holder.avgPrice.text = reviewDetail.avgPrice.toString()
+        holder.menuFishName.text = review.fishKind.toString()
+        holder.fishPrice.text = review.cost
+        holder.starScore.text = review.rating.toString()
+        holder.reviewContent.text = review.content
+        holder.likecounter.text = review.like.toString()
 
-
-
-        //평균값 계산ㅇ
-        val avgPrice = (reviewDetail.minPrice + reviewDetail.maxPrice) /2
-        holder.avgPrice.text = avgPrice.toString()
-
+//가격 및 멤버이름
+//        //멤버이름
+//        holder.memberName.text = user.nickname
+//
+//        //가격
+//        holder.minPrice.text = reviewDetail.minPrice.toString()
+//        holder.maxPrice.text = reviewDetail.maxPrice.toString()
+//        holder.avgPrice.text = reviewDetail.avgPrice.toString()
+//
+//
+//
+//        //평균값 계산ㅇ
+//        val avgPrice = (reviewDetail.minPrice + reviewDetail.maxPrice) /2
+//        holder.avgPrice.text = avgPrice.toString()
+//
         //점수에 따른 별 색깔 변화
-        val rating = reviewDetail.score!!.toFloat()
+        val rating = review.rating!!.toFloat()
         holder.ratingBar.rating = rating
 
-        //가격에 따른 문구 변화
-        val menuprice = reviewDetail.fishPrice
-        val avgprice = reviewDetail.avgPrice
-
-        val Textcomparison = if(menuprice > avgprice){
-            "평균가보다 높아요!"
-        }else{
-            "평균가보다 낮아요!"
-        }
-        holder.Textcomparison.text = Textcomparison
-
-        //가격에 따른 색깔변화
-        if(menuprice > avgprice){
-            holder.Textcomparison.setTextColor(holder.itemView.context.getColor(R.color.red))
-            holder.fishPrice.setTextColor(holder.itemView.context.getColor(R.color.red))
-        }else{
-            holder.Textcomparison.setTextColor(holder.itemView.context.getColor(R.color.blue))
-            holder.fishPrice.setTextColor(holder.itemView.context.getColor(R.color.blue))
-        }
+//        //가격에 따른 문구 변화
+//        val menuprice = reviewDetail.fishPrice
+//        val avgprice = reviewDetail.avgPrice
+//
+//        val Textcomparison = if(menuprice > avgprice){
+//            "평균가보다 높아요!"
+//        }else{
+//            "평균가보다 낮아요!"
+//        }
+//        holder.Textcomparison.text = Textcomparison
+//
+//        //가격에 따른 색깔변화
+//        if(menuprice > avgprice){
+//            holder.Textcomparison.setTextColor(holder.itemView.context.getColor(R.color.red))
+//            holder.fishPrice.setTextColor(holder.itemView.context.getColor(R.color.red))
+//        }else{
+//            holder.Textcomparison.setTextColor(holder.itemView.context.getColor(R.color.blue))
+//            holder.fishPrice.setTextColor(holder.itemView.context.getColor(R.color.blue))
+//        }
 
 
     }
 
 
     override fun getItemCount(): Int {
-        return detailReviews.size
+        return review.size
     }
 
 
--- app/src/main/java/com/example/eatraw/data/Review.kt
+++ app/src/main/java/com/example/eatraw/data/Review.kt
@@ -1,18 +1,15 @@
 package com.example.eatraw.data
 
 data class Review(
-    val content: String,
-    val marketName: String,
-    val storeImg: String?,
-    val storeName: String,
-    val rating: Double?,
-<<<<<<< HEAD
-    val region : String?
+    val content: String,    //리뷰
+    val marketName: String, //시장이름
+    val storeImg: String?,  //리뷰이미지(썸네일겸용)
+    val storeName: String,  //가게이름
+    val rating: Double?,    //별점
+    val region: String?,    //지역
+    val like: Int?,         //좋아요
+    val fishKind: Int?,     //고기종류
+    val cost: String?       //가격
 
-=======
-    val region: String?,
-    val like: Int?,
-    val fishKind: Int?,
-    val cost: String?
->>>>>>> c7ff4fba6eae846c2dd43a76ffe74710b7200ce8
+
 )
