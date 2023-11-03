f1f0c6e8d2218eec69f0aee2f7a502cec87904b8
zinc1004 <roes2003@naver.com>
2023-11-03 09:33:41 +0900

    11/03

--- app/src/main/java/com/example/eatraw/adapter/ReviewDetailAdapter.kt
+++ app/src/main/java/com/example/eatraw/adapter/ReviewDetailAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eatraw.R
 import com.example.eatraw.data.DetailReview
+
 
 class ReviewDetailAdapter(private val detailReviews: List<DetailReview>) :
     RecyclerView.Adapter<ReviewDetailAdapter.ReviewDetailAdapterViewHolder>() {
@@ -17,7 +19,16 @@ class ReviewDetailAdapter(private val detailReviews: List<DetailReview>) :
         val fishPrice : TextView = itemView.findViewById(R.id.StorePriceInt)
         val memberName : TextView = itemView.findViewById(R.id.mName)
         val starScore: TextView = itemView.findViewById(R.id.mStarsocore)
+        val ratingBar : RatingBar = itemView.findViewById(R.id.DratingBar)
         val contentView : TextView = itemView.findViewById(R.id.contentView)
+        val Textcomparison : TextView = itemView.findViewById(R.id.Textcomparison)
+
+        //가격
+        val minPrice : TextView = itemView.findViewById(R.id.IntMin)
+        val maxPrice : TextView = itemView.findViewById(R.id.IntMax)
+        val avgPrice : TextView = itemView.findViewById(R.id.IntAvg)
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int ): ReviewDetailAdapterViewHolder {
@@ -34,7 +45,43 @@ class ReviewDetailAdapter(private val detailReviews: List<DetailReview>) :
         holder.fishPrice.text = reviewDetail.fishPrice.toString()
         holder.memberName.text = reviewDetail.memberName
         holder.starScore.text = reviewDetail.score.toString()
-        holder.contentView.text = reviewDetail.textcontent
+        holder.contentView.text = reviewDetail.textcontent.toString()
+
+        holder.minPrice.text = reviewDetail.minPrice.toString()
+        holder.maxPrice.text = reviewDetail.maxPrice.toString()
+        holder.avgPrice.text = reviewDetail.avgPrice.toString()
+
+
+
+        //평균값 계산ㅇ
+        val avgPrice = (reviewDetail.minPrice + reviewDetail.maxPrice) /2
+        holder.avgPrice.text = avgPrice.toString()
+
+        //점수에 따른 별 색깔 변화
+        val rating = reviewDetail.score!!.toFloat()
+        holder.ratingBar.rating = rating
+
+        //가격에 따른 문구 변화
+        val menuprice = reviewDetail.fishPrice
+        val avgprice = reviewDetail.avgPrice
+
+        val Textcomparison = if(menuprice > avgprice){
+            "평균가보다 높아요!"
+        }else{
+            "평균가보다 낮아요!"
+        }
+        holder.Textcomparison.text = Textcomparison
+
+        //가격에 따른 색깔변화
+        if(menuprice > avgprice){
+            holder.Textcomparison.setTextColor(holder.itemView.context.getColor(R.color.red))
+            holder.fishPrice.setTextColor(holder.itemView.context.getColor(R.color.red))
+        }else{
+            holder.Textcomparison.setTextColor(holder.itemView.context.getColor(R.color.blue))
+            holder.fishPrice.setTextColor(holder.itemView.context.getColor(R.color.blue))
+        }
+
+
     }
 
 
