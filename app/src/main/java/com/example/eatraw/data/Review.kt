7fd466c07bdcd6196f7d3b1ddc83f142e926e1a9
DESKTOP-JJHHJ8M\admin <pys0938@naver.com>
2023-11-09 16:02:50 +0900

    박 이

--- app/src/main/java/com/example/eatraw/data/Review.kt
+++ app/src/main/java/com/example/eatraw/data/Review.kt
@@ -10,8 +10,8 @@ data class Review(
     val rating: Double?,
     val region: String?,
     val like: Int?,
-    val fishKind: String,
-    val cost: Int,
+    val fishKind: String?,
+    val cost: Int?,
     val userId: String?,  // 사용자 UID를 추가
     val reviewId: String? = null //  리뷰 고유 아이디 추가
 )
