37df5d0dd58d2ad6fe3d5ec2d7b9a75948970152
DESKTOP-JJHHJ8M\admin <pys0938@naver.com>
2023-11-10 16:30:09 +0900

    no message

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/eatraw/databinding/FragmentAlarmBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/eatraw/databinding/FragmentAlarmBinding.java
@@ -7,17 +7,24 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.eatraw.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentAlarmBinding implements ViewBinding {
   @NonNull
   private final FrameLayout rootView;
 
-  private FragmentAlarmBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final SwitchCompat alarmSwitch;
+
+  private FragmentAlarmBinding(@NonNull FrameLayout rootView, @NonNull SwitchCompat alarmSwitch) {
     this.rootView = rootView;
+    this.alarmSwitch = alarmSwitch;
   }
 
   @Override
@@ -43,10 +50,19 @@ public final class FragmentAlarmBinding implements ViewBinding {
 
   @NonNull
   public static FragmentAlarmBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.alarm_switch;
+      SwitchCompat alarmSwitch = ViewBindings.findChildViewById(rootView, id);
+      if (alarmSwitch == null) {
+        break missingId;
+      }
 
-    return new FragmentAlarmBinding((FrameLayout) rootView);
+      return new FragmentAlarmBinding((FrameLayout) rootView, alarmSwitch);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
