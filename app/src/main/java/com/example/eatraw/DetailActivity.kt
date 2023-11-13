bb74025fa07a69a53dfda02e6d800ca897633e34
DESKTOP-JJHHJ8M\admin <pys0938@naver.com>
2023-11-13 11:23:42 +0900

    내 리뷰 수정중

--- app/src/main/java/com/example/eatraw/DetailActivity.kt
+++ app/src/main/java/com/example/eatraw/DetailActivity.kt
@@ -10,7 +10,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.eatraw.databinding.ActivityDetailBoxBinding
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
 
